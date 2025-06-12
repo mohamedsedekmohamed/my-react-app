1375d4ba17669b2e27264f71fb68b2d6fcc79173
mohamedsedekmohamed <Mohamedsedek31@gmail.com>
2025-06-12 15:43:13 +0300

    q

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
 import './App.css'
 import Land from './Land'
+import { Routes, Route } from 'react-router-dom'
+import Suppert from './page/Suppert'
+import Privacy from './page/Privacy'
 function App() {
   
   return (
     <div className='relative max-w-screen'>
-      <Land/>
-    
+     <Routes>
+      <Route path="/" element={<Land />} />
+      <Route path="/Suppert" element={<Suppert />} />
+      <Route path="/Privacy" element={<Privacy />} />
+    </Routes>
 
     </div>
   )
